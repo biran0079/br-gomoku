106ccebb26afebc99990d76b6e8d300c14648cdf
Ran Bi <biran0079@gmail.com>
2015-08-11 00:19:21 -0700

    Fix winning mreesages.

--- src/view/UI.java
+++ src/view/UI.java
@@ -54,7 +54,7 @@ public class UI {
   }
 
   public void win(final Player p, Runnable restart){
-    String title = p.toString() + "wins!";
+    String title = p.toString() + " wins!";
     String message = "Do you want to start a new game?";
     SwingUtilities.invokeLater(() -> newDialog(title, message, restart));
   }
@@ -62,13 +62,14 @@ public class UI {
   private void newDialog(String title, String message, Runnable okCallback) {
     int n = JOptionPane.showOptionDialog(
         gameFrame,
-        message,
+        title + "\n" + message,
         title,
-        JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+        JOptionPane.YES_NO_OPTION,
+        JOptionPane.QUESTION_MESSAGE,
         null, // icon
         null, // options
         null); //initialValue
-    if(n == JOptionPane.OK_OPTION){
+    if(n == JOptionPane.YES_OPTION){
       okCallback.run();
     }
   }
